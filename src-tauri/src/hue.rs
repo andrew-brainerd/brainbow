85ad95252c8d31bb4ec84a97f3e16cab60c45722
Andrew Brainerd <abrainerd@zappos.com>
2024-12-08 13:48:37 -0500

    Add Hue event monitor

--- src-tauri/src/hue.rs
+++ src-tauri/src/hue.rs
@@ -1,13 +1,37 @@
-use sse_client::EventSource;
+use eventsource_client as event_client;
+use futures::{Stream, TryStreamExt};
 
-pub fn monitor_events() {
-    let event_source = EventSource::new("http://10.0.0.195/eventsource/clip/v2").unwrap();
+const HUE_URL: &str = "https://10.0.0.195/eventstream/clip/v2";
+const HUE_HEADER: &str = "hue-application-key";
+const HUE_KEY: &str = "k9ayxh7kwhoKodCqpcdh6vqGxZU3Cydg-4CkyFHu";
 
-    event_source.on_message(|message| {
-        println!("New message event {:?}", message);
-    });
+pub async fn monitor_events() -> Result<(), event_client::Error> {
+    let client = event_client::ClientBuilder::for_url(HUE_URL)
+        .unwrap()
+        .header(HUE_HEADER, HUE_KEY)
+        .unwrap()
+        .build();
 
-    event_source.add_event_listener("error", |error| {
-        println!("Error {:?}", error);
-    });
+    let mut stream = tail_events(client);
+
+    while let Ok(Some(_)) = stream.try_next().await {}
+
+    Ok(())
+}
+
+fn tail_events(client: impl event_client::Client) -> impl Stream<Item = Result<(), ()>> {
+    client
+        .stream()
+        .map_ok(|event| match event {
+            event_client::SSE::Connected(connection) => {
+                println!("Connected: \nstatus={}", connection.response().status())
+            }
+            event_client::SSE::Event(ev) => {
+                println!("Event: {}\n{}", ev.event_type, ev.data)
+            }
+            event_client::SSE::Comment(comment) => {
+                println!("Comment: \n{}", comment)
+            }
+        })
+        .map_err(|err| eprintln!("Error streaming events: {:?}", err))
 }
